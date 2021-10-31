b1b15be1ac3de1cd60eef6ec7205de42721683bf
Estevão <estevaom404@gmail.com>
2021-10-31 18:39:20 -0300

    Desenvolvendo Recursos [Usuário]

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,21 @@
 
 Route::group(['prefix' => 'admin', 'namespace' => 'admin', 'as' => 'admin.'], function (){
 
+    /** Formulário de Login */
     Route::get('/', 'AuthController@showLoginForm')->name('login');
     Route::post('login', 'AuthController@login')->name('login.do');
 
+    /** Rotas Protegidas */
     Route::group(['middleware' => ['auth']], function () {
+
+        /** Dashboard Home */
         Route::get('home', 'AuthController@home')->name('home');
+
+        Route::get('users/team', 'UserController@team')->name('users.team');
+        Route::resource('users', 'UserController');
     });
 
+    /** Logout */
     Route::get('logout', 'AuthController@logout')->name('logout');
 });
 
