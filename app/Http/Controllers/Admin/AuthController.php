4e48fa2bc3cbe374a1a18d9fcb91cc50ced622e5
Estevão <estevaom404@gmail.com>
2021-10-31 17:28:24 -0300

    capturando horário login e ip

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -9,7 +9,11 @@ use App\User;
 
 class AuthController extends Controller
 {
-    public function showLoginForm(){
+    public function showLoginForm()
+    {
+        if(Auth::check() === true) {
+            return redirect()->route('admin.home');
+        }
 
         return view('admin.index');
     }
@@ -40,6 +44,7 @@ class AuthController extends Controller
             return response()->json($json);
         }
 
+        $this->authenticated($request->getClientIp());
         $json['redirect'] = route('admin.home');
         return response()->json($json);
     }
@@ -48,6 +53,15 @@ class AuthController extends Controller
     {
         Auth::logout();
         return redirect()->route('admin.login');
+    }
+
+    private function authenticated(string $ip)
+    {
+        $user = User::where('id', Auth::user()->id);
+        $user->update([
+            'last_login_at' => date('Y-m-d H:i:s'),
+            'last_login_ip' => $ip
+        ]);
     }
 
 }
