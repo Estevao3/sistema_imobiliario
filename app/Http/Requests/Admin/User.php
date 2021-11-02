d4f8f545c36f4353f5031c432a9050b50b931b71
Estevão <estevaom404@gmail.com>
2021-11-02 18:41:36 -0300

    Validacâo dos campos

--- app/Http/Requests/Admin/User.php
+++ app/Http/Requests/Admin/User.php
@@ -25,7 +25,46 @@ class User extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|min:3|max:191'
+            'name' => 'required|min:3|max:191',
+            'genre' => 'in:male,female,other',
+            'document' => 'required|min:11|max:14|unique:users',
+            'document_secondary' => 'required|min:8|max:12',
+            'document_secondary_complement' =>  'required',
+            'date_of_birth' => 'required|date_format/d/m/y',
+            'place_of_birth' => 'required',
+            'civil_status' =>   'required|in:married,separated,single,divorced,widower',
+
+            //income
+            'occupation' => 'required',
+            'income' => 'required',
+            'company_work' => 'required',
+
+            //Address
+            'zipcode' => 'required|min:8|max:9',
+            'street' => 'required',
+            'number' => 'required',
+            'neighborhood' => 'required',
+            'state' => 'required',
+            'city' => 'required',
+
+            //contact
+            'cell' => 'required',
+
+            //Access
+            'email' => 'required|email|unique:users',
+
+            //spouse
+            'type_of_communion' => 'required_if:civil_status,married,separated|in:Comunhão Universal de Bens,Comunhão Parcial de Bens,Separação Total de Bens,Participação Final de Aquestos',
+            'spouse_name' => 'required_if:civil_status,married,separated|min:3|max:191',
+            'genre' => 'required_if:civil_status,married,separated|in:male,female,other',
+            'spouse_spouse_document' => 'required_if:civil_status,married,separated|min:11|max:14|unique:users',
+            'spouse_document_secondary' => 'required_if:civil_status,married,separated|min:8|max:12',
+            'spouse_document_secondary_complement' =>  'required_if:civil_status,married,separated',
+            'spouse_date_of_birth' => 'required_if:civil_status,married,separated|date_format/d/m/y',
+            'spouse_place_of_birth' => 'required_if:civil_status,married,separated',
+            'spouse_occupation' => 'required_if:civil_status,married,separated',
+            'spouse_income' => 'required_if:civil_status,married,separated',
+            'spouse_company_work' => 'required_if:civil_status,married,separated',
         ];
     }
 }
